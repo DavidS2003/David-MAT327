4b728a60b6b0043bf88456ce9cc73edd2b29da7a
DavidS2003 <127816723+DavidS2003@users.noreply.github.com>
2024-10-10 23:04:09 -0400

    Add files via upload

---
+++ Milestone 5.R
@@ -0,0 +1,18 @@
+#Hour column
+  #Mean 
+    mean(filtered_data$Hour) # 12.73407
+  #Median 
+    median(filtered_data$Hour) # 13
+  #Variance
+    var(filtered_data$Hour) # 39.67814
+  #Standard deviation
+    sd(filtered_data$Hour) # 6.299058
+#Number of Persons Killed column
+    #Mean 
+      mean(filtered_data[["NUMBER OF PERSONS KILLED"]]) # 0.002846968
+    #Median 
+      median(filtered_data[["NUMBER OF PERSONS KILLED"]]) # 0
+    #Variance
+      var(filtered_data[["NUMBER OF PERSONS KILLED"]]) # 0.003087357
+    #Standard deviation
+      sd(filtered_data[["NUMBER OF PERSONS KILLED"]]) # 0.05556399
